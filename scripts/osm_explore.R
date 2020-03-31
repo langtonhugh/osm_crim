4fc1d048551ff2673c7eb41a78c2d46d91d6b51b
Sam Langton <36928709+langtonhugh@users.noreply.github.com>
2020-03-31 16:18:51 +0100

    More tests

--- scripts/osm_explore.R
+++ scripts/osm_explore.R
@@ -10,7 +10,7 @@ library(tidyr)
 # an area using the latitude and longitude coordinates, but it is generally
 # easier to use a search term.
 
-bb <- getbb(place_name = "manchester united kingdom", format_out = "sf_polygon") 
+bb <- getbb(place_name = "greater london united kingdom", format_out = "sf_polygon") 
 
 p <- opq(bbox = bb) %>%                                           # select bounding box
   add_osm_feature(key = 'amenity', value = 'bicycle_parking') %>% # select features
@@ -27,12 +27,12 @@ p.sf  <- st_transform(p.sf, 27700)
 bb.sf <- st_transform(bb, 27700)
 p.clip.sf <- st_intersection(bb.sf, p.sf)
 
-ggplot() +
-  geom_sf(data = bb) +
-  geom_sf(data = p.clip.sf, size = 0.5)
+# ggplot() +
+#   geom_sf(data = bb) +
+#   geom_sf(data = p.clip.sf, size = 0.5)
 
 # crime data
-crime.df <- read_csv("data/2019-01-greater-manchester-street.csv")
+crime.df <- read_csv("data/2020-01-metropolitan-street.csv")
 
 bike.crime.sf <- crime.df %>% 
   filter(`Crime type` == "Bicycle theft") %>% 
@@ -45,5 +45,19 @@ bike.crime.sf <- crime.df %>%
 ggplot() +
   geom_sf(data = bb) +
   geom_sf(data = p.clip.sf, size = 0.8) +
-  geom_sf(data = bike.crime.sf, size = 0.8, col = "red", alpha = 0.6)
+  geom_sf(data = bike.crime.sf, size = 0.8, col = "red")
 
+# polygons
+bb <- getbb(place_name = "coventry united kingdom", format_out = "sf_polygon") 
+
+p <- opq(bbox = bb) %>%                                        
+  add_osm_feature(key = 'public_transport', value = 'station') %>% 
+  osmdata_sf() %>% 
+  trim_osmdata(bb_poly = bb)
+
+p
+p.sf <- p$osm_polygons
+
+ggplot() +
+  geom_sf(data = bb) +
+  geom_sf(data = p.sf, fill = "black")
