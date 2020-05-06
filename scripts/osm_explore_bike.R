71368b494e76391cd786de8e0965cc5aba80183a
Sam Langton <36928709+langtonhugh@users.noreply.github.com>
2020-05-06 15:17:11 +0100

    Edits and additions 3

--- scripts/osm_explore_bike.R
+++ scripts/osm_explore_bike.R
@@ -4,75 +4,31 @@ library(sf)
 library(readr)
 library(dplyr)
 library(tidyr)
+library(jsonlite)
 
-# All queries begin with a bounding box specification i.e. the study region.
-# This can be obtained manually, which requires some existing knowledge about
-# an area using the latitude and longitude coordinates, but it is generally
-# easier to use a search term. For instance
+bb_sf <- getbb(place_name = "greater london", format_out = "sf_polygon") 
 
-bb <- getbb(place_name = "greater london", format_out = "sf_polygon") 
+osm_bike_sf <- opq(bbox = bb_sf) %>%                                  # select bounding box.
+  add_osm_feature(key = 'amenity', value = 'bicycle_rental') %>%      # select keys and values.
+  osmdata_sf() %>%                                                    # specify class (sf or sp).
+  trim_osmdata(bb_poly = bb_sf)                                       # trim by the study region.   
 
-p <- opq(bbox = bb) %>%                                           # select bounding box
-  add_osm_feature(key = 'amenity', value = 'bicycle_rental') %>%
-  osmdata_sf() %>%                                                    # specify class (sf or sp)
-  trim_osmdata(bb_poly = bb)   
+osm_bike_sf
+osm_bike_sf <- osm_bike_sf$osm_points # extract point only
 
-p
-p.sf <- p$osm_points # extract point only
+osm_bike_sf <- osm_bike_sf %>% 
+  filter(brand == "Santander Cycles")
 
-ggplot() +
-  geom_sf(data = bb) +
-  geom_sf(data = p.sf)
+# TfL API
+tfl_bike_sf <- api_call <- fromJSON(readLines("https://api.tfl.gov.uk/bikepoint"))
 
-tfl_bike <- api_call <- fromJSON(readLines("https://api.tfl.gov.uk/bikepoint"))
+# Make spatial.
+tfl_bike_sf <- tfl_bike_sf %>%
+  st_as_sf(coords = c(x = "lon", y = "lat"), crs = 4326) 
 
-tfl_bike_sf <- tfl_bike %>% 
-  st_as_sf(coords = c(x = "lon", y = "lat"), crs = 4326)
-
-
-
-p.sf <- st_transform(p.sf, 4326)
-
+# Plot comparison.
 ggplot() + 
-  geom_sf(data = bb) +
-  geom_sf(data = tfl_bike_sf) +
-  geom_sf(data = p.sf, col = "Red", alpha = 0.2)
-  
-  
-ggplot(p.sf) + geom_sf()
+  geom_sf(data = bb_sf) +
+  geom_sf(data = osm_bike_sf) +
+  geom_sf(data = tfl_bike_sf, col = "red", alpha = 0.3)
 
-ggplot() +
-  geom_sf(data = bb) +
-  geom_sf(data = tfl_bike_sf) +
-
-
-# crime data
-crime.df <- read_csv("data/2020-01-metropolitan-street.csv")
-
-bike.crime.sf <- crime.df %>% 
-  filter(`Crime type` == "Bicycle theft") %>% 
-  drop_na(Longitude, Latitude) %>% 
-  st_as_sf(coords = c(x = "Longitude", y = "Latitude"), crs = 4326) %>% 
-  st_transform(27700) %>% 
-  st_intersection(bb.sf)
-
-# plot
-ggplot() +
-  geom_sf(data = bb) +
-  geom_sf(data = p.clip.sf, size = 0.8) +
-  geom_sf(data = bike.crime.sf, size = 0.8, col = "red")
-
-# polygons
-bb <- getbb(place_name = "coventry united kingdom", format_out = "sf_polygon") 
-
-p <- opq(bbox = bb) %>%                                        
-  add_osm_feature(key = 'public_transport', value = 'station') %>% 
-  osmdata_sf() %>% 
-  trim_osmdata(bb_poly = bb)
-
-p
-p.sf <- p$osm_polygons
-
-ggplot() +
-  geom_sf(data = bb) +
-  geom_sf(data = p.sf, fill = "black")
--- scripts/osm_explore_tube.R
+++ scripts/osm_explore_tube.R
@@ -6,8 +6,7 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(purrr)
-library(cowplot) # maybe don't show?
-library(stringr)
+library(patchwork)
 
 # OSM query ==============================================================================================
 
@@ -31,50 +30,64 @@ osm_stat_sf <- osm_stat_sf$osm_points
 
 # Check the network tags.
 table(osm_stat_sf$network)
-table(osm_stat_sf$operator)
 
-# Filter any that include "London Underground".
-osm_tube_sf <- osm_stat_sf %>% 
-  filter(str_detect(network, "London Underground"))
+# Filter Northern line only/
+osm_north_sf <- osm_stat_sf %>% 
+  filter(line == "Northern")
 
-osm_tube_sf <- st_transform(osm_tube_sf, 27700)
+osm_north_sf <- st_transform(osm_north_sf, 27700)
 bb_sf      <- st_transform(bb_sf, 27700)
 
 ggplot() +
   geom_sf(data = bb_sf) +
-  geom_sf(data = osm_tube_sf, mapping = aes(colour = line))
+  geom_sf(data = osm_north_sf, size = 0.8)
 
 # TfL scrape ==============================================================================================
 
-# Scape bus stops on route 24 from TfL. Warning is fine.
-api_call <- fromJSON(readLines("https://api.tfl.gov.uk/Line/Meta/Modes"))
-
-
-
-api_call <- fromJSON(readLines("https://api.tfl.gov.uk/line/northern/route/sequence/outbound"))
+# Scape northern line locations from TfL. Warning is fine.
+api_call <- fromJSON(readLines("https://api.tfl.gov.uk/line/northern/stoppoints"))
 
 # Extract bus stop names and the lat-long coordinates, transform to BNG.
-tfl_bus91_df <- api_call %>% 
-  pluck("stations") %>% 
-  select(name, lat, lon) %>% 
+tfl_north_sf <- api_call %>% 
+  select(commonName, lat, lon) %>% 
   st_as_sf(coords = c(x = "lon", y = "lat"), crs = 4326) %>% 
   st_transform(27700)
 
 # Plot difference.
 ggplot() +
-  geom_sf(data = tfl_bus91_df, size = 2) +
-  geom_sf(data = osm_bus_sf, col = "red", alpha = 0.5) +
-  theme_minimal()
+  geom_sf(data = osm_north_sf) +
+  geom_sf(data = tfl_north_sf, color = "red", alpha = 0.5) 
 
-# Keep only one of the OSM records. This is a scenario where we don't have the 'real' alternative.
-osm_bus91_dup_sf <- osm_bus91_sf %>% 
-  group_by(name) %>% 
-  slice(1)
+# Assessing the impact in terms of crime. BTP data in January 2020.
+btp_df <- read_csv("data/2020-01-btp-street.csv")
 
-# Plot difference.
-ggplot() +
-  geom_sf(data = tfl_bus91_df, size = 2) +
-  geom_sf(data = osm_bus91_dup_sf, col = "red", alpha = 0.8) +
-  theme_minimal()
+# Make spatial.
+btp_sf <- btp_df %>%
+  drop_na(Longitude, Latitude) %>%
+  st_as_sf(coords = c(x = "Longitude", y = "Latitude"), crs = 4326) %>%
+  st_transform(27700) %>%
+  st_intersection(bb_sf)
 
+# Create buffers to define 'in and around'.
+osm_buff_sf <- st_buffer(osm_north_sf, dist = 50)
+tfl_buff_sf <- st_buffer(tfl_north_sf, dist = 50)
+
+# Aggregate to each station.
+osm_north_sf <- osm_buff_sf %>% 
+  mutate(crimes = lengths(st_intersects(osm_buff_sf, btp_sf)))
+
+tfl_north_sf <- tfl_buff_sf %>% 
+  mutate(crimes = lengths(st_intersects(tfl_buff_sf, btp_sf)))
+
+# Map out difference.
+p1 <- ggplot(data = osm_north_sf) +
+  geom_sf(mapping = aes(colour = crimes), size = 3) +
+  labs(title = "Open Street Map") +
+  scale_colour_viridis_c()
   
+p2 <- ggplot(data = tfl_north_sf) +
+  geom_sf(mapping = aes(colour = crimes), size = 3) +
+  labs(title = "Transport for London") +
+  scale_colour_viridis_c()
+
+p1 + p2
